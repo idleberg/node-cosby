c992d9036bff31878217cbca24dd2be5313f2bc1
Jan T. Sott <git@idleberg.com>
2025-10-31 18:42:53 +0100

    feat: add log message about used config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
+import { relative } from 'node:path';
+import process from 'node:process';
 import { loadConfig, type UserInputConfig } from 'c12';
 import type { OptionValues } from 'commander';
+import consola from 'consola';
 
 export async function loadEsbuildConfig(options: OptionValues) {
 	const result: UserInputConfig = await loadConfig({
@@ -16,6 +19,9 @@ export async function loadEsbuildConfig(options: OptionValues) {
 	if (!result._configFile) {
 		throw Error('No configuration file found.');
 	}
+
+	console.log(/* let it breathe */);
+	consola.info(`Using configuration ${relative(process.cwd(), result._configFile)}`);
 
 	return result.config;
 }
